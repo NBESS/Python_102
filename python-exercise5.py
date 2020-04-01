d80fa81d282ce181a5772140fc552b6fb4136769
NBESS <nicholasdbess@gmail.com>
2020-04-01 15:25:33 -0400

    adds user input to code

---
+++ python-exercise5.py
@@ -0,0 +1,10 @@
+# Get user to input a number, then convert the number to a string. 
+# Tell the user to go to work or sleep in based on day of week.
+
+# Capture users input into a variable
+day = int(input('Day (0-6)? '))
+
+# Output if user should Go to work or sleep in
+
+
+
