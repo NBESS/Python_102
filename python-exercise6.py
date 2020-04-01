ec263e1e8ab86fb6bf781426d419ff2269800927
NBESS <nicholasdbess@gmail.com>
2020-04-01 15:47:58 -0400

    adds code to convert C to F

---
+++ python-exercise6.py
@@ -0,0 +1,12 @@
+# Convert the user's input of temperature in celsius to fahrenheit, and display output
+
+# User's input in celsius
+temp_in_c = int(input('Temperature in C? '))
+
+
+# Convert to fahrenhit
+temp_in_f = (temp_in_c * (5/9)) + 32
+
+message = f'{temp_in_f} F'
+
+# Output in fahrenheit
